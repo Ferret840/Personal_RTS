1be06238d12bedb3bff07a9e9386066e8f9fe18a
Ferret840 <clameyer95@gmail.com>
2019-10-29 02:29:03 -0700

    FlowField Pathfinding P2 (Basic Subsector)
    Intermediate completion of sector/subsector generation for process of completely refactoring pathfinding to use sectors/sub-sectors and flow fields.
    
    Added Logger for writing logs to file for debugging in cases where Unity may become unresponsive.
    Created seperate material for each dimension.
    Implemented first step of subsectors, which are generated inside each sector. Currently only 1 subsector exists and contains all nodes, but will later be generating 1 subsector per connected region of nodes within a sector.
    Changed terrain debug display to generate 2D mesh based on area covered by each subsector.
    Implemented more debugging functions for terrain, such as a slower debugger for 1 sector at a time which draws cubes on each walkable tile within that sector.

--- Personal_RTS/Assets/Scripts/Node.cs
+++ Personal_RTS/Assets/Scripts/Node.cs
@@ -7,61 +7,19 @@ public class Node
     ///<summary>
     ///Always 0, 1, 2, or 3. Set bits indicate walkable in that dimension
     ///</summary>
-    char walkable;
-    public Vector3 worldPosition;
-
-    public Node(char _blocked, Vector3 _worldPos)
+    /*public bool isWalkable
     {
-        walkable = _blocked;
-        worldPosition = _worldPos;
+        get;
+        private set;
     }
 
-    public void SetWalkable(char newWalkable)
+    public Node(bool _isWalkable)
     {
-        walkable = newWalkable;
-
-        if (newWalkable != (char)0)
-        {
-            
-        }
-        else
-        {
-
-        }
+        isWalkable = _isWalkable;
     }
 
-    /// <summary>
-    /// Takes a dimension and returns True if this Node can be traversed by that dimension.
-    /// </summary>
-    /// <param name="dimension">The dimension to be checked against as a LayerMask. 2^8, 2^9, 2^10)</param>
-    /// <returns>Returns true if the node can be traversed by the given dimension.</returns>
-    public bool GetWalkableByDimension(LayerMask dimension)
+    public bool SetWalkable(bool _newIsWalkable)
     {
-        char walkableBool = (char)0;
-
-        if ((dimension & (1 << 8)) != 0)
-        {
-            walkableBool |= (char)1;
-        }
-        else if ((dimension & (1 << 9)) != 0)
-        {
-            walkableBool |= (char)2;
-        }
-        else if ((dimension & (1 << 10)) != 0)
-        {
-            if (walkable == (char)3)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        return (walkable & walkableBool) != 0;
-    }
-
-    public char GetWalkable()
-    {
-        return walkable;
-    }
+        return isWalkable = _newIsWalkable;
+    }*/
 }
--- Personal_RTS/Assets/Scripts/Sector.cs
+++ Personal_RTS/Assets/Scripts/Sector.cs
@@ -11,39 +11,53 @@ public class Sector
     }
 
     //Table of node locations. 1 = Walkable, 0 = Impassable terrain
-    bool[,] grid;
-
-    List<SubSector> subs;
-
-    public Sector(LayerMask dim, int xSize, int ySize, Vector3 cornerPos, float nodeRadius)
+    public Node[,] grid
     {
+        get;
+        private set;
+    }
+    int xSize, ySize;
+
+    public List<SubSector> subs
+    {
+        get;
+        private set;
+    }
+
+    public Sector(LayerMask dim, int _xSize, int _ySize, Vector3 cornerPos, float nodeDiameter)
+    {
+        xSize = Mathf.RoundToInt(_xSize / nodeDiameter);
+        ySize = Mathf.RoundToInt(_ySize / nodeDiameter);
+
         Color c = Random.ColorHSV();
         c.a = 0.5f;
         sectorColor = c;
 
-        grid = new bool[xSize, ySize];
-
+        grid = new Node[xSize, ySize];
+        
         for (int x = 0; x < xSize; ++x)
         {
             for (int y = 0; y < ySize; ++y)
             {
-                grid[x, y] = true;
+                //grid[x, y] = new Node(true);
+                bool walkable = true;
 
-                Vector3 worldPoint = cornerPos + Vector3.right * (x * nodeRadius * 2) + Vector3.forward * (y * nodeRadius * 2);
+                Vector3 worldPoint = cornerPos + Vector3.right * (x * nodeDiameter) + Vector3.forward * (y * nodeDiameter);
 
                 Ray ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
 
-                //Dimension
+                //Dimension terrain block detection
                 RaycastHit[] hits = Physics.RaycastAll(ray, 100, dim);
                 foreach (RaycastHit h in hits)
                 {
                     if (h.transform.gameObject.tag == "Unit")
                         continue;
 
-                    grid[x, y] = false;
+                    //grid[x, y].SetWalkable(false);
+                    walkable = false;
                     break;
                 }
-                if (dim == 1 << 8 || dim == 1 << 9)
+                if (walkable/*grid[x, y].isWalkable*/ && (dim == 1 << 8 || dim == 1 << 9))
                 {
                     hits = Physics.RaycastAll(ray, 100, 1 << 10);
                     foreach (RaycastHit h in hits)
@@ -51,26 +65,276 @@ public class Sector
                         if (h.transform.gameObject.tag == "Unit")
                             continue;
 
-                        grid[x, y] = false;
+                        //grid[x, y].SetWalkable(false);
+                        walkable = false;
                         break;
                     }
                 }
+
+                if (walkable)
+                {
+                    grid[x, y] = new Node();
+                }
             }
         }
+
+        UpdateSubsectors();
+    }
+
+    public void UpdateSubsectors()
+    {
+        System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+        timer.Start();
+
+        subs = new List<SubSector>();
+        subs.Add(new SubSector(this));
+
+        for (int x = 0; x < xSize; ++x)
+        {
+            for (int y = 0; y < ySize; ++y)
+            {
+                if (grid[x,y] != null)//grid[x, y].isWalkable)
+                    subs[0].AddNodeToSub(grid[x, y]);
+            }
+        }
+
+        foreach(SubSector s in subs)
+            s.GenerateMesh();
+
+        //Debug.Log(string.Format("Generated Subsectors for Sector {0} in {1}ms", this.ToString(), timer.ElapsedMilliseconds));
+        timer.Stop();
     }
 
     public bool GetWalkableAt(int x, int y)
     {
-        return grid[x, y];
+        return grid[x, y] != null;//.isWalkable;
     }
 
     public bool SetWalkableAt(int x, int y, bool newCanWalk)
     {
-        return grid[x, y] = newCanWalk;
+        if (newCanWalk && grid[x, y] == null)
+            grid[x, y] = new Node();
+        else if (!newCanWalk)
+            grid[x, y] = null;
+        return newCanWalk;//grid[x, y].SetWalkable(newCanWalk);
     }
 
-    private class SubSector
+    //Subsector Class
+    public class SubSector
     {
+        Sector sect;
+        HashSet<Node> NodesInSubsector = new HashSet<Node>();
 
+        public Mesh mesh
+        {
+            get;
+            private set;
+        }
+
+        List<Vector2> verts = new List<Vector2>();
+
+        Node[,] grid
+        {
+            get
+            {
+                return sect.grid;
+            }
+        }
+
+        public Color subSectorColor
+        {
+            get;
+            private set;
+        }
+
+        public SubSector(Sector parentSector)
+        {
+            sect = parentSector;
+
+            Color c = Random.ColorHSV();
+            c.a = 0.5f;
+            subSectorColor = c;
+        }
+
+        public void AddNodeToSub(Node n)
+        {
+            NodesInSubsector.Add(n);
+        }
+
+        public void GenerateMesh()
+        {
+            verts = new List<Vector2>();
+
+            if (NodesInSubsector.Count <= 1)
+                try
+                {
+                    mesh = Resources.Load<GameObject>("CubeMesh").GetComponent<MeshFilter>().sharedMesh;
+                }
+                catch { return; }
+            else
+                mesh = new Mesh();
+
+            int x = 0, y = 0;
+            bool broke = false;
+            for (; x < sect.xSize; ++x)
+            {
+                for (y = 0; y < sect.ySize; ++y)
+                {
+                    if (NodesInSubsector.Contains(grid[x, y]))
+                    {
+                        broke = true;
+                        break;
+                    }
+                }
+                if (broke)
+                    break;
+            }
+
+            if (broke == false)
+            {
+                Debug.Log(string.Format("Error: Subsector {0} contains no nodes", this.ToString()));
+                return;
+            }
+
+            verts.Add(new Vector2(x + 0.5f, y + 0.5f));
+            CheckNext(x, y, 1, 1);
+            verts = RemoveRedundantVerts(verts);
+            
+            Triangulator tr = new Triangulator(verts);
+            int[] indices = tr.Triangulate();
+
+            // Create the Vector3 vertices
+            Vector3[] vertices = new Vector3[verts.Count];
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                vertices[i] = new Vector3(verts[i].x, verts[i].y, 0);
+            }
+
+            mesh.vertices = vertices;
+            mesh.triangles = indices;
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+            
+            verts = null;
+        }
+
+        List<Vector2> RemoveRedundantVerts(List<Vector2> verts)
+        {
+            for (int x = 1; x < verts.Count - 1; ++x)
+            {
+                while (x < verts.Count - 1 &&
+                        ((verts[x - 1].x == verts[x].x && verts[x].x == verts[x + 1].x) ||
+                         (verts[x - 1].y == verts[x].y && verts[x].y == verts[x + 1].y))   )
+                {
+                    verts.RemoveAt(x);
+                }
+            }
+            if (verts.Count >= 3 &&
+                ((verts[0].x == verts[verts.Count - 1].x && verts[verts.Count - 1].x == verts[verts.Count - 2].x) ||
+                (verts[0].x == verts[verts.Count - 1].y && verts[verts.Count - 1].y == verts[verts.Count - 2].y))    )
+            {
+                verts.RemoveAt(verts.Count - 1);
+            }
+
+            return verts;
+        }
+
+        //Extreme logic
+        void CheckNext(int x, int y, int xN, int yM)
+        {
+            int sameLimit = 0;
+            while (sameLimit < 4)
+            {
+                int nextX = x + xN, nextY = y + yM;
+
+                ++sameLimit;
+
+                if (xN == yM)
+                {
+                    xN = 0;
+                    nextX = x;
+
+                    //Check for left and right bounds
+                    if (nextY < 0)
+                    {
+                        xN = -1;
+                        yM = 1;
+                        //CheckNext(x, y, -1, 1);
+                        continue;
+                    }
+                    else if (nextY >= sect.ySize)
+                    {
+                        xN = 1;
+                        yM = -1;
+                        //CheckNext(x, y, 1, -1);
+                        continue;
+                    }
+                }
+                else
+                {
+                    yM = 0;
+                    nextY = y;
+
+                    //Check for bottom and top bounds
+                    if (nextX < 0)
+                    {
+                        xN = 1;
+                        yM = 1;
+                        //CheckNext(x, y, 1, 1);
+                        continue;
+                    }
+                    else if (nextX >= sect.xSize)
+                    {
+                        xN = -1;
+                        yM = -1;
+                        //CheckNext(x, y, -1, -1);
+                        continue;
+                    }
+                }
+
+                //Get node to check
+                Node n = grid[nextX, nextY];
+
+                //If this isn't in the subsector
+                if (!NodesInSubsector.Contains(n))
+                {
+                    if (xN == 0)
+                    {
+                        yM = -(xN = yM);
+                    }
+                    else
+                    {
+                        yM = xN = -xN;
+                    }
+
+                    //Check next clockwise for this same node as origin
+                    continue;
+                    //CheckNext(x, y, xN, yM);
+                }
+                else
+                {
+                    Vector2 nextVert = new Vector2(nextX, nextY) + Vector2.one * 0.5f;
+                    if (verts[0] == nextVert)
+                        return;
+                    verts.Add(nextVert);
+
+                    if (xN == 0)
+                    {
+                        xN = -yM;
+                    }
+                    else
+                    {
+                        yM = xN;
+                    }
+
+                    //Check next counter-clockwise
+                    x = nextX;
+                    y = nextY;
+                    sameLimit = 0;
+                    continue;
+                    //CheckNext(nextX, nextY, xN, yM);
+                }
+            }
+        }
     }
 }
